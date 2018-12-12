c48e4c2b7625199d5ad03584fa54e252f9748dd6
mangdehuang <1032036512@qq.com>
2018-12-12 20:57:48 +0800

    init

---
+++ src/components/Content.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="content"></div>
+</template>
+<script>
+import MoviceItem from "./MoviceItem";
+export default {
+  name: "Content",
+  data() {
+    return {};
+  },
+  props: {
+    moviceList: {}
+  },
+  components: {
+    MoviceItem
+  }
+};
+</script>
+<style lang="">
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Home">
+    <el-container>
+      <el-header>{{title}} 数目：{{allData.count}}</el-header>
+      <Content :moviceList="moviceList"></Content>
+    </el-container>
+  </div>
+</template>
+<script>
+import Content from "./Content";
+export default {
+  name: "Home",
+  data() {
+    return {
+      title: "",
+      moviceList: [],
+      allData: {}
+    };
+  },
+  created() {
+    this.$http.get("api/v2/movie/in_theaters").then(res => {
+      console.log(res.data);
+      this.allData = res.data;
+      this.title = res.data.title;
+      this.moviceList = res.data.subjects;
+    });
+  },
+  components: {
+    Content
+  }
+};
+</script>
+<style lang="">
+</style>
